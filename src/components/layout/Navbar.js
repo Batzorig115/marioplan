8c65b2ce3a28a0d7dfab2f69f0ed42ee15a623ca
batzorig.enkh <batzorig.enkh@gmail.com>
2019-12-09 07:31:02 +0800

    user connect firebase auth

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import SignInLinks from './SignInLinks';
 import SignOutLinks from './SignOutLinks';
+import { connect } from 'react-redux';
 
 const Navbar = () => {
     return (
@@ -14,5 +15,10 @@ const Navbar = () => {
         </nav>
     )
 }
+const mapStateToProps = (state) => {
+    return {
 
-export default Navbar;
+    }
+}
+
+export default connect(mapStateToProps)(Navbar);
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,6 +1,23 @@
-const initState = {}
+const initState = {
+    authError : null
+}
 
 const authReducer = ( state = initState, action ) => {
+    switch(action.type){
+        case 'LOGIN_ERROR':
+            return {
+                ...state,
+                authError: 'Login failed'
+            }
+        case 'LOGIN_SUCCESS':
+            console.log('login success');
+            return {
+                ...state,
+                authorError: null
+            }
+        default:
+            return state;
+    }
     return state;
 }
 
